d6ac007987bec04071ba1bb21e42c7ea16fc75ad
Tiakowski <tiagosantosrazr@gmail.com>
2022-03-15 22:08:09 -0300

    Testanto OO

--- Contas/CriarConta.py
+++ Contas/CriarConta.py
@@ -1,26 +1,36 @@
-def gerar_numero():
-    import random
-    numero = str(random.randrange(100000))
-    conta ="C" + numero
-    return conta
-
-def conferir_existencia(conta):
-   contas = open("contas.txt","r")
-   verificador = contas.read()
-   existe = conta in verificador
-   contas.close()
-   return existe
-
-def conta_nova():
-    existe = True
-    while existe:
-        conta = gerar_numero()
-        existe = conferir_existencia(conta)
-    return conta
-
 def criar_conta_nova():
+
+    def gerar_numero():
+        import random
+        numero = str(random.randrange(100000))
+        conta = "C" + numero
+        return conta
+
+    def conferir_existencia(conta):
+        try:
+            contas = open("contas.txt", "r")
+            verificador = contas.read()
+            existe = conta in verificador
+            contas.close()
+            return existe
+        except FileNotFoundError:
+            contas = open("contas.txt", 'w')
+            conferir_existencia(conta)
+
+    def conta_nova():
+        existe = True
+        while existe:
+            conta = gerar_numero()
+            existe = conferir_existencia(conta)
+        return conta
+
+
     conta = conta_nova()
-    with open("contas.txt","a") as contas:
+    with open("contas.txt", "a") as contas:
         contas.write("\n")
         contas.write(conta)
     return conta
+
+
+conta = criar_conta_nova()
+print(conta)
