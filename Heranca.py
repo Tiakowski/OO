92526b7acfac3c29bc18d133e79de5836541d049
Tiakowski <tiagosantosrazr@gmail.com>
2022-03-18 00:43:41 -0300

    lista

--- Heranca.py
+++ Heranca.py
@@ -11,6 +11,11 @@ class Programa:
     def dar_likes(self):
         self._likes += 1
 
+    def horas(self, duracao):
+        horas = int(self.duracao / 60)
+        minutos = int(self.duracao % 60)
+        return f'{horas}h{minutos}min'
+
     @property
     def nome(self):
         return self._nome
@@ -19,19 +24,30 @@ class Programa:
     def nome(self, nome):
         self._nome = nome
 
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano}, -  Duração: {Programa.horas(self,self.duracao)}'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
-        super().__init__(nome,ano)
+        super().__init__(nome, ano)
         self.temporadas = temporadas
 
-batman = Filme('the batman', 2022, 176)
-print(f'Nome: {batman.nome} - Ano: {batman.ano} - Duração: {int(batman.duracao / 60)}h{int(batman.duracao % 60)}min')
-# m = int(digitado - hora * 60)
+    def __str__(self):
+        return f'Nome: {self.nome} - Ano: {self.ano} - Temporadas: {self.temporadas}'
 
+
+batman = Filme('the batman', 2022, 176)
+bastardos = Filme('bastardos inglorios', 2009, 153)
 peacemaker = Serie('peacemaker', 2022, 1)
-print(f'Nome: {peacemaker.nome} - Ano: {peacemaker.ano}')
+
+lista = [batman, bastardos, peacemaker]
+
+for programa in lista:
+    print(programa)
